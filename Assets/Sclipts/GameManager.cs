7afc3a0f43263e51cafe13065805743233fdc6a4
FutoAoi <Faoi0803@icloud.com>
2025-10-11 20:21:17 +0900

    [Add]デバックルームの作成、IDamageableインターフェイスを追加、FPS視点の機能を追加

--- Assets/Sclipts/GameManager.cs
+++ Assets/Sclipts/GameManager.cs
@@ -1,16 +1,30 @@
-using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private List<ICharactor> _charactorList = new List<ICharactor>();
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        var charactors = FindObjectsByType<MonoBehaviour>(
+            FindObjectsInactive.Include,     // 非アクティブも含める
+            FindObjectsSortMode.None         // ソートなし（速い）
+        ).OfType<ICharactor>();
+        foreach (var charactor in charactors)
+        {
+            _charactorList.Add(charactor);
+            charactor.SetupCharactor();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        foreach(ICharactor charactor in _charactorList)
+        {
+            charactor.UpdateCharactor();
+        }
     }
 }
--- Assets/Sclipts/PlayerController.cs
+++ Assets/Sclipts/PlayerController.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour, ICharactor
+public class PlayerController : MonoBehaviour, ICharactor, IDamageable
 {
     [SerializeField,Tooltip("最大体力")] float _maxHp;
     [SerializeField,Tooltip("最大マナ")] float _maxMp;
     [SerializeField,Tooltip("移動速度")] float _moveSpeed;
+
+    [Header("カメラ設定")]
+    [SerializeField] GameObject _mainCamera;
+    [SerializeField] float _xSensitivity, _ySensitivity;
+    [SerializeField] float _maxCameraAngle;
 
     private float _currentHp;
     private float _currentMp;
@@ -12,13 +17,18 @@ public class PlayerController : MonoBehaviour, ICharactor
     private Vector3 _move;
     private Rigidbody _rb;
 
+    float _xRot, _yRot;
+    float _clampYRot;
+    Quaternion _playerRot;
+
     //プロパティ
     public float HP => _currentHp;
     public float MoveSpeed => _moveSpeed;
 
-    public void Setup()
+    public void SetupCharactor()
     {
         _rb = GetComponent<Rigidbody>();
+        _playerRot = transform.localRotation;
         _currentHp = _maxHp;
         _currentMp = _maxMp;
     }
@@ -26,6 +36,7 @@ public class PlayerController : MonoBehaviour, ICharactor
     public void UpdateCharactor()
     {
         Move();
+        FPSCameraMove();
     }
 
     public void Die()
@@ -43,10 +54,26 @@ public class PlayerController : MonoBehaviour, ICharactor
         _x = Input.GetAxis("Horizontal");
         _y = Input.GetAxis("Vertical");
 
+        if (Mathf.Abs(_x) < 0.1f) _x = 0f;
+        if (Mathf.Abs(_y) < 0.1f) _y = 0f;
+
         _move = transform.right * _x + transform.forward * _y;
         _move.Normalize();
 
         _rb.MovePosition(_rb.position + _move * _moveSpeed * Time.deltaTime);
+    }
 
+    private void FPSCameraMove()
+    {
+        _xRot = Input.GetAxis("Mouse X") * _xSensitivity;
+        _yRot = Input.GetAxis("Mouse Y") * _ySensitivity;
+
+        _clampYRot -= _yRot;
+        _clampYRot = Mathf.Clamp(_clampYRot, -_maxCameraAngle, _maxCameraAngle);
+
+        _playerRot *= Quaternion.Euler(0f, _xRot, 0f);
+
+        _mainCamera.transform.localRotation = Quaternion.Euler(_clampYRot, 0f, 0f);
+        transform.localRotation = _playerRot;
     }
 }
